88ed4f04ca7aafea147d067f8734e301330188d3
Neil Nakade <neilnakade18@gmail.com>
2026-03-24 14:58:52 +0530

    dynamic vector db for cloud

--- app/retrieval/retriever.py
+++ app/retrieval/retriever.py
@@ -1,20 +1,19 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from app.ingestion.loader import load_documents
+from app.ingestion.splitter import split_documents
 
-
-def retrieve_documents(query, k=5):
-
+def retrieve_documents(query, k=3):
     embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
 
-    vector_store = FAISS.load_local(
-        "vector_db",
-        embeddings,
-        allow_dangerous_deserialization=True
-    )
+    # 🔥 Create vector DB dynamically
+    documents = load_documents("data/raw")
+    chunks = split_documents(documents)
+
+    vector_store = FAISS.from_documents(chunks, embeddings)
 
     results = vector_store.similarity_search_with_score(query, k=k)
-
     return results
     
