9a2cf9cbe33d4968d5e9a0bbae6cfe6c87e2373d
Neil Nakade <neilnakade18@gmail.com>
2026-03-24 14:32:23 +0530

    fixed embeddings for cloud

--- app/retrieval/retriever.py
+++ app/retrieval/retriever.py
@@ -1,10 +1,12 @@
-from langchain_ollama import OllamaEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
 
 def retrieve_documents(query, k=5):
 
-    embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
 
     vector_store = FAISS.load_local(
         "vector_db",
